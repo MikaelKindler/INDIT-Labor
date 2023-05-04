9d48742e70299c185a40f0167606e01e62be77a7
MikaelKindler <132549164+MikaelKindler@users.noreply.github.com>
2023-05-04 10:31:03 +0200

    Add files via upload

---
+++ LAB_INDIT_1.1.py
@@ -0,0 +1,17 @@
+a="Hello"
+b="World"
+print(a+b)
+
+x = int(input("Zahl 1:"))
+y = int(input("Zahl 2:"))
+print(x+y)
+print(x*y)
+print(x/y)
+print(x-y)
+print("Ergebnis der Addition:",x+y)
+
+
+
+
+
+
